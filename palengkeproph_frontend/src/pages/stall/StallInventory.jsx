eea0a5607dd82bc4e163db3916e201545774a31d
bcharles4 <95261817+bcharles4@users.noreply.github.com>
2025-12-09 11:42:28 +0800

    change

--- palengkeproph_frontend/src/pages/stall/StallInventory.jsx
+++ palengkeproph_frontend/src/pages/stall/StallInventory.jsx
@@ -549,7 +549,7 @@ export default function StallInventory() {
       id: `B-${String(i + 1).padStart(0, "0")}`,
       original_owner: "Charles",
       type: "Food",
-      classification: "Upper Admin Block",
+      section: "Upper Admin Block",
       location: `B${i + 1}`,
       dimensions: "2x3m",
       capacity: 1,
