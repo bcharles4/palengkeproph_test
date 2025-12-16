631eb58e424cbcbfc6d6ee17fa385cfcc6b6d929
bcharles4 <95261817+bcharles4@users.noreply.github.com>
2025-12-16 09:53:36 +0800

    yo

--- palengkeproph_backend/palengkeproph_backend/settings.py
+++ palengkeproph_backend/palengkeproph_backend/settings.py
@@ -65,7 +65,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
-    'DEFAULT_PERMISSION_CLASSES': [
+    'DEFAULT_PERMISSION_CLASSES':[
         'rest_framework.permissions.IsAuthenticated',
     ],
 }
